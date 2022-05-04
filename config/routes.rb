3edcf3d7e95c077ac89c7dbc283eae3a34121129
mlandrum22 <mlandrum22@gmail.com>
2022-05-03 20:28:19 -0700

    more eats to rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'restaurants' => 'eats#index', as: 'eats'
+  get 'restaurants/new' => 'eats#new', as: 'new_eat'
+  #get 'restaurants/:id' => 'eats#show' as: 'restaurant'
+  #get 'restaurants/edit':id
+ 
   resources :adventures
   resources :eats
   root 'home#index'
 
   get 'about' => 'home#about', as: 'about'
+
 end
